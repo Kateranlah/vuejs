39ca35d18946f1fdbd48cb6d55d7d4362fbd965f
Kevin Sven Lentz <kevin.s.lentz@web.de>
2023-10-02 16:07:50 +0200

    started edditing card

--- first-vue/src/components/BurgerMenu.vue
+++ first-vue/src/components/BurgerMenu.vue
@@ -1,7 +1,29 @@
 <template>
-    <div class="card-container"  @click.prevent="closeMenu">
+    <div class="card-container" @click.prevent="closeMenu">
         <div class="card" @click.stop="">
+            <div class="card__top">
+                <h2>Mein Account</h2>
+                <img :src="('/first-vue/src/assets/img/burger-menu.png')">
+            </div>
+            <!-- //Work in progress. Just Fillers without function -->
+            <div class="card__butto-container">
+                <button>Anmelden</button>
+                <button>Account erstellen</button>
+            </div>
 
+            <div class="card__content">
+                <!-- make as list items -->
+                <ul>
+                    <li>
+                        <router-link class="link" to="/">
+                            <img class="icon pr1" :src="('/first-vue/src/assets/img/company.png')">
+                            Bestellungen</router-link>
+                    </li>
+                </ul>
+
+
+            </div>
+            <!-- //The top is Work in progress. Just Fillers without function -->
         </div>
     </div>
 </template>

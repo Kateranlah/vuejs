fd1dd9f7dd71e0f97b7520c6ecb9f35ff253c471
Kevin Sven Lentz <kevin.s.lentz@web.de>
2023-10-06 12:06:36 +0200

    change concept of responsiveness eyecatcher

--- first-vue/src/views/Landing.vue
+++ first-vue/src/views/Landing.vue
@@ -1,12 +1,12 @@
 <template>
     <main>
         <section class="first-section f-row m-f-row">
-            <div>
+            <div class="f-column eyecatcher eyecatcher__left">
                 <h1>Bestelle Essen und mehr</h1>
                 <h2>Restaurants und Geschäfte, die zu dir liefern</h2>
                 <div><input type="text"></div>
             </div>
-            <div class="f-column eyecatcher">
+            <div class="f-column eyecatcher eyecatcher__right">
                 <img class="main-img" src="../assets/img/fruitplate.png" alt="" srcset="">
                 <span class="logo-text"><i>GREEN</i> FOOD</span>
             </div>

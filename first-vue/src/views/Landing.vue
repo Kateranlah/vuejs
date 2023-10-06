79460fc2a0bd5460d5a99b3ff40afcb6d2e17780
Kevin Sven Lentz <kevin.s.lentz@web.de>
2023-10-06 16:27:33 +0200

    started designing the searchbar

--- first-vue/src/views/Landing.vue
+++ first-vue/src/views/Landing.vue
@@ -4,13 +4,59 @@
             <div class="f-column eyecatcher eyecatcher__left">
                 <h1>Bestelle Essen und mehr</h1>
                 <h2>Restaurants und Geschäfte, die zu dir liefern</h2>
-                <div><input type="text"></div>
+                <div class="search-container">
+                    <div class="__input-container">
+                        <span class="location-icon-container">
+                            <img class="icon" src="../assets/img/location.png">
+                        </span>
+                        <label for="">
+                            <input type="text" placeholder="Adresse, z.B. Musterstr. 1"></label>
+                        <span>
+                            <span class="cancel-icon-container"><img class="icon" src="../assets/img/x.png"></span>
+                        </span>
+                    </div>
+                    <span>
+                        <div class="search-icon-container">
+                            <img class="icon" src="../assets/img/magnifying-glass.png">
+                            <!-- <span> Suche </span> -->
+                        </div>
+                    </span>
+                </div>
+
             </div>
             <div class="f-column eyecatcher eyecatcher__right">
                 <img class="main-img" src="../assets/img/fruitplate.png" alt="" srcset="">
                 <span class="logo-text"><i>GREEN</i> FOOD</span>
             </div>
         </section>
+        <!-- <section>
+            <div>
+                <h3>So bestellst Du</h3>
+                <h2>Es ist ganz einfach</h2>
+            </div>
+
+
+            <div>
+                <div>
+                    <img src="">
+                    <h5>Teile uns deinen Standort mit</h5>
+                    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptatem amet, totam magni ex at ri totam
+                    </p>
+                </div>
+                <div>
+                    <img src="">
+                    <h5>Suche, worauf du Lust hast</h5>
+                    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptatem amet, totam magni ex at ri totam
+                    </p>
+                </div>
+                <div>
+                    <img src="">
+                    <h5>Bestelle zur Lieferung oder Abholung</h5>
+                    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptatem amet, totam magni ex at ri totam
+                    </p>
+                </div>
+            </div>
+        </section> -->
     </main>
 </template>
 

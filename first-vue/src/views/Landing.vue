e75bbfc5dd1998bb0dba93505564571cd97ba389
Kevin Sven Lentz <kevin.s.lentz@web.de>
2023-10-20 16:29:33 +0200

    worked on slider

--- first-vue/src/views/Landing.vue
+++ first-vue/src/views/Landing.vue
@@ -31,31 +31,36 @@
                 <h3 class="normal">So bestellst Du</h3>
                 <h2>Es ist ganz einfach.</h2>
             </div>
-
-            <div class="f-row --max-w-1024">
-                <div v-if="desktopView || choosen == 0" class="card-simple">
-                    <div><img src="../assets/img/location.png"></div>
-                    <h5>Teile uns deinen Standort mit</h5>
-                    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptatem amet, totam magni ex at ri totam
-                    </p>
-                </div>
-                <div v-if="desktopView || choosen == 1" class="card-simple">
-                    <div><img src="../assets/img/apple.png"></div>
-                    <h5>Suche, worauf du Lust hast</h5>
-                    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptatem amet, totam magni ex at ri totam
-                    </p>
-                </div>
-                <div v-if="desktopView || choosen == 2" class="card-simple">
-                    <div><img src="../assets/img/bike.png"></div>
-                    <h5>Bestelle zur Lieferung oder Abholung</h5>
-                    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptatem amet, totam magni ex at ri totam
-                    </p>
+            <div id="slider-container">
+                <div ref="test" class="f-row --max-w-1024 slider">
+                    <div v-if="desktopView || choosen == 0" class="card-simple">
+                        <div><img src="../assets/img/location.png"></div>
+                        <h5>Teile uns deinen Standort mit</h5>
+                        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptatem amet, totam magni ex at ri
+                            totam
+                        </p>
+                    </div>
+                    <div v-if="desktopView || choosen == 0" class="card-simple">
+                        <div><img src="../assets/img/apple.png"></div>
+                        <h5>Suche, worauf du Lust hast</h5>
+                        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptatem amet, totam magni ex at ri
+                            totam
+                        </p>
+                    </div>
+                    <div v-if="desktopView || choosen == 0" class="card-simple">
+                        <div><img src="../assets/img/bike.png"></div>
+                        <h5>Bestelle zur Lieferung oder Abholung</h5>
+                        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptatem amet, totam magni ex at ri
+                            totam
+                        </p>
+                    </div>
                 </div>
             </div>
-            <span id="dot-container"> 
-               <div @click="choosen = 0" class="dot bg-g"></div>
-               <div @click="choosen = 1" class="dot"></div>
-               <div @click="choosen = 2" class="dot"></div>
+
+            <span id="dot-container">
+                <div @click="choosen = 0" class="dot bg-g"></div>
+                <div @click="choosen = 1" class="dot"></div>
+                <div @click="choosen = 2" class="dot"></div>
             </span>
         </section>
     </main>
@@ -67,20 +72,18 @@ import "../styles/views/landing.scss"
 let choosen = ref(0)
 
 
-
-
 // repetetive code. reduce it 
 import { ref } from "vue";
 let desktopView = ref()
 
-function myFunction(x:any) {
-  if (x.matches) {
-    desktopView.value = false;
-  } else {
-    desktopView.value = true;
-  }
-  console.log(desktopView.value);
-  
+function myFunction(x: any) {
+    if (x.matches) {
+        desktopView.value = false;
+    } else {
+        desktopView.value = true;
+    }
+    console.log(desktopView.value);
+
 }
 let x = window.matchMedia("(max-width: 1025px)")
 myFunction(x) // Call listener function at run time
